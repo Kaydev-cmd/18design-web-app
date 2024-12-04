d9655536132980883d3c72171b0db48717316c11
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2024-12-04 10:25:01 -0800

    I made a few code refactors

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
-// import React from "react";
+import React from "react";
 import "./Home.css";
+import PricingCards from "../../components/PricingCards";
 
-// Ticket #3: Handle the responsiveness for the pricing and common section and the footer!!
 // Ticket #4: Check button transitions for buttons throughout the project!!
 // Ticket #5: Change the bullet points for the pricing cards to squares!!
 // Ticket #6: Check font families for the text throughout the project!!
@@ -53,7 +53,7 @@ const Home = () => {
         <div className="container">
           <div className="about-us-wrapper">
             <img src="/images/home/Illustration_2.png" alt="About us banner" />
-            <div className="about-us-right-wrapper">
+            <div className="right-wrapper">
               <button className="about-us-btn">About Us</button>
               <h1>
                 Faster, friendlier feedback
@@ -193,74 +193,7 @@ const Home = () => {
               </p>
             </div>
           </div>
-
-          {/* Pricing Cards */}
-          <div className="pricing-cards">
-            <div className="container">
-              <div className="pricing-cards-wrapper">
-                <div className="pricing-card white first-card">
-                  <div className="amount-and-plan">
-                    <h5>
-                      <span>$</span>
-                      <h6>49</h6>
-                      <p>per month</p>
-                    </h5>
-                  </div>
-                  <p>A pay-once license, just for you</p>
-                  <div className="card-list">
-                    <ul>
-                      <li>The Mac app - yours to keep</li>
-                      <li>Beautiful White_level</li>
-                      <li>One year of saving to Cloud</li>
-                    </ul>
-                  </div>
-                  <button>Join</button>
-                </div>
-
-                <div className="pricing-card black">
-                  <div className="amount-and-plan">
-                    <h5>
-                      <span>$</span>
-                      <h6>99</h6>
-                      <p>per month</p>
-                    </h5>
-                  </div>
-                  <p>Pro account, just for you</p>
-                  <div className="card-list">
-                    <ul>
-                      <li>The Mac app - yours to keep</li>
-                      <li>One year of Mac app updates</li>
-                      <li>One year of saving to Cloud</li>
-                      <li>A personal Cloud workspace</li>
-                      <li>All tools, just for you</li>
-                    </ul>
-                  </div>
-                  <button>Join</button>
-                </div>
-
-                <div className="pricing-card white last-card">
-                  <div className="amount-and-plan">
-                    <h5>
-                      <span>$</span>
-                      <h6>299</h6>
-                      <p>per month</p>
-                    </h5>
-                  </div>
-                  <p>A subscription for the whole team</p>
-                  <div className="card-list">
-                    <ul>
-                      <li>Access to the Mac app for all</li>
-                      <li>A shared Cloud workspace</li>
-                      <li>Free Cloud access to unlim</li>
-                      <li>Easy team management</li>
-                      <li>No license key required</li>
-                    </ul>
-                  </div>
-                  <button>Join</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <PricingCards />
         </div>
       </section>
 
